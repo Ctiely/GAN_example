7f9b5596c37c72bf1036f0ce95afe4553d903a67
Ctiely <814868906@qq.com>
2019-05-25 11:36:35 +0800

    modify

--- dcgan.py
+++ dcgan.py
@@ -19,10 +19,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s|%(levelname)s|%(message)s')
 
 def lrelu(x, leak=0.2, name="lrelu"):
-    with tf.variable_scope(name):
-        f1 = 0.5 * (1 + leak)
-        f2 = 0.5 * (1 - leak)
-        return f1 * x + f2 * abs(x)
+    return tf.maximum(x, leak*x)
     
 def sample_z(size, z_dim):
     return np.random.normal(0, 1, size=[size, z_dim])
@@ -60,19 +57,20 @@ class DCGAN(Base):
                     self.z, height * width * 1024,
                     activation_fn=lrelu, normalizer_fn=tcl.batch_norm)
             g = tf.reshape(g, (-1, height, width, 1024))
+            g = tf.nn.relu(tcl.batch_norm(g))
             
             g = tcl.conv2d_transpose(
-                    g, 512, 3, stride=2, activation_fn=lrelu,
+                    g, 512, 3, stride=2, activation_fn=tf.nn.relu,
                     normalizer_fn=tcl.batch_norm, padding="SAME",
                     weights_initializer=tf.random_normal_initializer(0, 0.02))
             
             g = tcl.conv2d_transpose(
-                    g, 256, 3, stride=2, activation_fn=lrelu,
+                    g, 256, 3, stride=2, activation_fn=tf.nn.relu,
                     normalizer_fn=tcl.batch_norm, padding="SAME",
                     weights_initializer=tf.random_normal_initializer(0, 0.02))
             
             g = tcl.conv2d_transpose(
-                    g, 128, 3, stride=2, activation_fn=lrelu,
+                    g, 128, 3, stride=2, activation_fn=tf.nn.relu,
                     normalizer_fn=tcl.batch_norm, padding="SAME",
                     weights_initializer=tf.random_normal_initializer(0, 0.02))
             
@@ -104,31 +102,29 @@ class DCGAN(Base):
             
             d = tcl.fully_connected(shared, 1, activation_fn=None, 
                                     weights_initializer=tf.random_normal_initializer(0, 0.02))
-            q = tcl.fully_connected(shared, 128, activation_fn=lrelu, normalizer_fn=tcl.batch_norm)
-            q = tcl.fully_connected(q, 2, activation_fn=None)
             
-            return d, q
+            return tf.nn.sigmoid(d), d
         
         with tf.variable_scope("discriminator"):
-            self.D_real, _ = __discriminator(self.image)
+            self.D, self.D_logits = __discriminator(self.image)
             
         with tf.variable_scope("discriminator", reuse=True):
-            self.D_fake, _ = __discriminator(g)
+            self.D_, self.D_logits_ = __discriminator(g)
         
     def _build_algorithm(self):
-        G_optimizer = tf.train.AdamOptimizer(self.lr, epsilon=1e-5)
-        D_optimizer = tf.train.AdamOptimizer(self.lr, epsilon=1e-5)
+        G_optimizer = tf.train.AdamOptimizer(self.lr, beta1=0.5, epsilon=1e-5)
+        D_optimizer = tf.train.AdamOptimizer(self.lr, beta1=0.5, epsilon=1e-5)
         
         D_trainable_variables = tf.trainable_variables("discriminator")
         G_trainable_variables = tf.trainable_variables("generator")
         real_loss = tf.reduce_mean(
-                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_real, labels=tf.ones_like(self.D_real)))
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits, labels=tf.ones_like(self.D)))
         fake_loss = tf.reduce_mean(
-                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_fake, labels=tf.zeros_like(self.D_fake)))
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits_, labels=tf.zeros_like(self.D_)))
         self.D_loss = real_loss + fake_loss
         
         self.G_loss = tf.reduce_mean(
-                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_fake, labels=tf.ones_like(self.D_fake)))
+                tf.nn.sigmoid_cross_entropy_with_logits(logits=self.D_logits_, labels=tf.ones_like(self.D_)))
     
         # clip gradients
         D_grads = tf.gradients(self.D_loss, D_trainable_variables)
@@ -138,10 +134,10 @@ class DCGAN(Base):
         G_clipped_grads, _ = tf.clip_by_global_norm(G_grads, self.max_grad_norm)
         
         self.D_solver = D_optimizer.apply_gradients(
-                zip(D_clipped_grads, D_trainable_variables), global_step=tf.train.get_global_step())
+                zip(D_clipped_grads, D_trainable_variables))
         
         self.G_solver = G_optimizer.apply_gradients(
-                zip(G_clipped_grads, G_trainable_variables), global_step=tf.train.get_global_step())
+                zip(G_clipped_grads, G_trainable_variables))
     
     def _generator(self, datas, batch_size):
         n_sample = len(datas)
@@ -161,7 +157,7 @@ class DCGAN(Base):
         for i, sample in enumerate(G_samples):
             cv2.imwrite(f"{save_dir}/step{step}/image{i}.png", (sample + 1) / 2.0 * 255)
     
-    def train(self, datas, training_steps=int(1e6), batch_size=32, G_updates=1):
+    def train(self, datas, training_steps=int(1e6), batch_size=64, G_updates=2):
         assert G_updates > 0
         step = 1
         while True:
